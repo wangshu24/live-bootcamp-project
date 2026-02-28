c23201914a6d6f5cd30d4601b4cbff7ebc216355
wangshu24 <trandinhphuc1997@gmail.com>
2026-02-28 08:45:50 -0500

    [sprint 1][task 3] finished structuring lib crate for shared auth instance

--- auth-service/src/main.rs
+++ auth-service/src/main.rs
@@ -3,21 +3,13 @@ use tower_http::services::ServeDir;
 
 #[tokio::main]
 async fn main() {
-    let assets_dir = ServeDir::new("assets");
-    let app = Router::new()
-        .fallback_service(assets_dir)
-        .route("/hello", get(hello_handler));
+    let app = Application::build("0.0.0.0:3000")
+            .await
+            .expect("Failed to build app");
 
-    // Here we are using ip 0.0.0.0 so the service is listening on all the configured network interfaces.
-    // This is needed for Docker to work, which we will add later on.
-    // See: https://stackoverflow.com/questions/39525820/docker-port-forwarding-not-working
-    let listener = tokio::net::TcpListener::bind("0.0.0.0:3000").await.unwrap();
-    println!("listening on {}", listener.local_addr().unwrap());
-
-    axum::serve(listener, app).await.unwrap();
+    app.run().await.expect("Failed to run app");
 }
 
 async fn hello_handler() -> Html<&'static str> {
-    // TODO: Update this to a custom message!
     Html("<h1>Hello, welcome to this little corner of the intraweb!</h1>")
 }
